abd9c78a44b36a1a77a59da88c32e50ed322008f
Gligor <dracevo.kromid@gmail.com>
2017-05-31 00:24:05 +0200

    Finish bubble sort by

--- sort.rb
+++ sort.rb
@@ -18,9 +18,38 @@ def bubble_sort(ary)
     end
     n -= 1
   end
-  print ary
+  print ary.to_s + "\n" # .to_s + "\n" IS OPTIONAL
 end
 
 bubble_sort([4,3,78,2,0,2])
 
 #DONE
+
+
+#BUBBLE SORT BY ALGORITHM
+
+def bubble_sort_by(ary)
+  #copied from upstairs at bubble_sort
+  n = ary.length - 1
+  while n > 0 do
+    index = 0
+
+    n.times do
+      first = ary[index] #first = left
+      second = ary[index+1] # second = right practically
+
+       if yield(first, second) > 0 #ONLY LINE THAT IS CHANGED
+         ary[index+1] = first
+         ary[index] = second
+       end
+
+      index += 1
+    end
+    n -= 1
+  end
+  print ary
+end
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+  left.length - right.length
+end
