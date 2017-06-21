93f187e15db2de701af95883d4bd85b156507ac9
Gligor <dracevo.kromid@gmail.com>
2017-06-21 11:59:11 +0200

    Add my count and my map

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,6 +1,7 @@
 module Enumerable
 
   def my_each
+    return self unless block_given?
     ary = self.to_a
     index = 0
     ary.length.times do
@@ -10,6 +11,7 @@ module Enumerable
   end
 
   def my_each_with_index
+    return self unless block_given?
     ary = self.to_a
     index = 0
     ary.length.times do
@@ -19,53 +21,80 @@ module Enumerable
   end
 
   def my_select
+    result = Array.new
     self.my_each do |item|
-      puts item if yield(item) == true
+      result << item if yield(item) == true
     end
+    result
   end
 
   def my_all?
-    ary_check = Array.new
-    ary_org = self.to_a
-    self.my_each do |item|
-      ary_check.push(item) if yield(item) == true
+    if block_given? #like...whatever?
+      ary_check = Array.new
+      ary_org = self.to_a
+      self.my_each do |item|
+        ary_check.push(item) if yield(item) == true
+      end
+      puts ary_org.length == ary_check.length ? "true" : "false"
+      #STRINGS JUST TO CONFIRM, WILL NEED TO CHANGE SHIT LATER ANYWAY
+      return ary_org.length == ary_check.length ? true : false
+      #THIS IS THE REAL THING
+
+      #CHANGE ON ANY/NONE TOO
     end
-    puts ary_org.length == ary_check.length ? "true" : "false"
-    #STRINGS JUST TO CONFIRM, WILL NEED TO CHANGE SHIT LATER ANYWAY
-    return ary_org.length == ary_check.length ? true : false
-    #THIS IS THE REAL THING
-
-    #CHANGE ON ANY/NONE TOO
-
   end
 
   def my_any?
     #COPIED
-    ary_check = Array.new
-    self.my_each do |item|
-      ary_check.push(item) if yield(item) == true
-    end
-    puts ary_check.length > 0 ? "true" : "false"
-    #STRINGS JUST TO CONFIRM, WILL NEED TO CHANGE SHIT LATER ANYWAY
-    return ary_check.length > 0 ? true : false
+    if block_given? #like...whatever?
+      ary_check = Array.new
+      self.my_each do |item|
+        ary_check.push(item) if yield(item) == true
+      end
+      puts ary_check.length > 0 ? "true" : "false"
+      #STRINGS JUST TO CONFIRM, WILL NEED TO CHANGE SHIT LATER ANYWAY
+      return ary_check.length > 0 ? true : false
 
-    #CHANGE ON ALL/NONE TOO
+      #CHANGE ON ALL/NONE TOO
+    end
   end
 
   def my_none?
     #COPIED
-    ary_check = Array.new
-    ary_org = self.to_a
-    self.my_each do |item|
-      ary_check.push(item) if yield(item) == true
-    end
-    puts ary_check.length == 0 ? "true" : "false"
-    #STRINGS JUST TO CONFIRM, WILL NEED TO CHANGE SHIT LATER ANYWAY
-    return ary_check.length == 0 ? true : false
+    if block_given? #like...whatever?
+      ary_check = Array.new
+      ary_org = self.to_a
+      self.my_each do |item|
+        ary_check.push(item) if yield(item) == true
+      end
+      puts ary_check.length == 0 ? "true" : "false"
+      #STRINGS JUST TO CONFIRM, WILL NEED TO CHANGE SHIT LATER ANYWAY
+      return ary_check.length == 0 ? true : false
 
-    #CHANGE ON ALL/ANY TOO
+      #CHANGE ON ALL/ANY TOO
+    end
   end
 
+  def my_count(obj=nil) #ona koga go cekas rezultatot dole a go dava gore...
+    if block_given?
+      counter = 0
+      self.my_each { |item| counter += 1 if yield(item) == true }
+       #TRY TO INTEGRATE DIRECTLY THROUGH SELECT LATER
+    elsif obj != nil
+      counter = 0
+      self.my_each { |item| counter += 1 if item == obj }
+    else
+      counter = self.length
+    end
+    counter
+  end
+
+  def my_map
+    return self unless block_given?
+    result = Array.new
+    self.my_each { |item| result << yield(item)}
+    result # Although not visible in the results i THINK it returns an array
+  end
 
 end
 
@@ -73,41 +102,48 @@ end
 
 array = [1,2,3,4,5, "string"]
 
-array.my_each do |item|
-  puts item
-end
+hash = {
+  "key1" => "val1",
+  "key2" => "val2"
+}
 
-array.my_each_with_index do |item, index|
-  puts item.to_s + " " + index.to_s
-end
+puts "---my each---"
 
-array.my_select do |item|
-  item.is_a? String
-end
+puts "--ary--"
+array.my_each { |item| puts item }
 
+puts "--hash--"
+hash.each { |k,v| puts k.to_s + ".." + v.to_s }
+
+puts "---my each with index---"
+array.my_each_with_index { |item, index| puts item.to_s + " " + index.to_s }
+
+puts "---my select---(forced puts for clarity)"
+array.my_select { |item| puts item if item.is_a? String }
+
+puts "---my all, any, none---(forced puts for clarity again)"
 test_array = [1,2,3,4,5]
 test_array2 = [1,2,3,4,"string"]
 
-test_array.my_all? do |item|
-  item.is_a? Integer
-end
+test_array.my_all? { |item| item.is_a? Integer }
 
-test_array2.my_all? do |item|
-  item.is_a? Integer
-end
+test_array2.my_all? { |item| item.is_a? Integer }
 
-test_array.my_any? do |item|
-  item.is_a? String
-end
+test_array.my_any? { |item| item.is_a? String }
 
-test_array2.my_any? do |item|
-  item.is_a? String
-end
+test_array2.my_any? { |item| item.is_a? String }
 
-test_array.my_none? do |item|
-  item.is_a? String
-end
+test_array.my_none? { |item| item.is_a? String }
 
-test_array2.my_none? do |item|
-  item.is_a? String
-end
+test_array2.my_none? { |item| item.is_a? String }
+
+puts "---my count---"
+puts "--obj=nil--"
+puts array.my_count
+puts "--obj!=nil--"
+puts array.my_count(2)
+puts "--block_given? == true--"
+puts array.my_count {|item| item%2 == 0}
+
+puts "---my map---"
+puts array.my_map {|item| item += 1 if item.is_a? Integer}
