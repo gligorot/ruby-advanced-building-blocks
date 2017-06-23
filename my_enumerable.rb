39aae2efe65216de2a3c1834d15f6a71c5e98aaf
Gligor <dracevo.kromid@gmail.com>
2017-06-23 13:22:58 +0200

    Add final, practically finished

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -23,7 +23,7 @@ module Enumerable
   def my_select
     result = Array.new
     self.my_each do |item|
-      result << item if yield(item) == true
+      result << item if yield(item)
     end
     result
   end
@@ -33,7 +33,7 @@ module Enumerable
       ary_check = Array.new
       ary_org = self.to_a
       self.my_each do |item|
-        ary_check.push(item) if yield(item) == true
+        ary_check.push(item) if yield(item)
       end
       puts ary_org.length == ary_check.length ? "true" : "false"
       #STRINGS JUST TO CONFIRM, WILL NEED TO CHANGE SHIT LATER ANYWAY
@@ -49,7 +49,7 @@ module Enumerable
     if block_given? #like...whatever?
       ary_check = Array.new
       self.my_each do |item|
-        ary_check.push(item) if yield(item) == true
+        ary_check.push(item) if yield(item)
       end
       puts ary_check.length > 0 ? "true" : "false"
       #STRINGS JUST TO CONFIRM, WILL NEED TO CHANGE SHIT LATER ANYWAY
@@ -65,7 +65,7 @@ module Enumerable
       ary_check = Array.new
       ary_org = self.to_a
       self.my_each do |item|
-        ary_check.push(item) if yield(item) == true
+        ary_check.push(item) if yield(item)
       end
       puts ary_check.length == 0 ? "true" : "false"
       #STRINGS JUST TO CONFIRM, WILL NEED TO CHANGE SHIT LATER ANYWAY
@@ -78,7 +78,7 @@ module Enumerable
   def my_count(obj=nil) #ona koga go cekas rezultatot dole a go dava gore...
     if block_given?
       counter = 0
-      self.my_each { |item| counter += 1 if yield(item) == true }
+      self.my_each { |item| counter += 1 if yield(item) }
        #TRY TO INTEGRATE DIRECTLY THROUGH SELECT LATER
     elsif obj != nil
       counter = 0
@@ -92,39 +92,71 @@ module Enumerable
   def my_map
     return self unless block_given?
     result = Array.new
-    self.my_each { |item| result << yield(item)}
-    result # Although not visible in the results i THINK it returns an array
+    self.my_each { |item| result << yield(item) }
+    result.pop(1)
+    print result
   end
 
+
+  def my_map_proc(&p)
+    return self unless block_given?
+    result = Array.new
+    self.my_each { |item| result << yield(item) }
+    result.pop(1)
+    print result
+  end
+
+  def my_map_proc_block(prok=nil)
+    result = Array.new
+    if prok.class == Proc
+      self.my_each { |item| result << prok.call(item)}
+    elsif block_given?
+      self.my_each { |item| result << yield(item)} #PROBLEM lies here, ruyb detects both proc and block as given (2 blocks) and thus returns an error....cant figure it out, fuck this
+    else
+      self
+    end
+    result
+    print result
+  end
+
+
   def my_inject(*arg)
-    #2.send(:+.to_s,3) -- EXAMPLE
+    ary = self.to_a
     if block_given?
       if arg.size == 0 #no initial given
-
+        result = 0 #need to make a * or / checker ...i really dont want to spend any more time on this
+        ary.my_each do |item|
+          result = yield(result, item)
+        end
+        result
       elsif arg.size == 1 #initial given
-
+        result = arg[0]
+        ary.my_each do |item|
+          result = yield(result, item)
+        end
+        result
       end
     else
-      ary = self.to_a
       if arg.size == 1 #only sym given
-        #result =0/1 determinator
         arg[0].to_s == "+" || arg[0].to_s == "-" ? result = 0 : result = 1
-
-        ary.each do |item|
-          result = result.send(arg[0], item)
-        end
+        ary.my_each { |item| result = result.send(arg[0], item) }
 
       elsif arg.size == 2 #initial, sym given
         result = arg[0]
-        ary.each do |item|
-          result = result.send(arg[1], item)
-        end
+        ary.my_each { |item| result = result.send(arg[1], item) }
       end
       result
     end
   end
 
+
+
 end
+
+def multiply_els(ary)
+  return ary.my_inject(1) { |product, n| product * n }
+end
+
 #2.send(:+.to_s,3)
 
 #==============TESTS BELOW THIS LINE =============
@@ -174,13 +206,27 @@ puts array.my_count(2)
 puts "--block_given? == true--"
 puts array.my_count {|item| item%2 == 0}
 
+p = Proc.new {|item| item += 1 if item.is_a? Integer}
+
 puts "---my map---"
+puts "--normal--"
 puts array.my_map {|item| item += 1 if item.is_a? Integer}
+puts "--proc mod--"
+puts array.my_map_proc(&p) # p is defined ABOVE MY MAP
+puts "--block-proc--"
+puts array.my_map_proc_block(&p) {|item| item += 2 if item.is_a? Integer}
+
 
 puts "---my inject---"
+
 puts "--block given--"
 puts "-init given-"
+puts (5..10).my_inject(1) { |product, n| product * n }
 puts "-init not given-"
+puts (5..10).my_inject {|sum, n| sum + n }
+puts (5..10).my_inject { |product, n| product * n }
+puts "^error"
+
 puts "--block not given--"
 puts "-init not given-"
 puts (5..10).my_inject(:+)
@@ -188,3 +234,6 @@ puts (5..10).my_inject(:*)
 puts "-init given-"
 puts (5..10).my_inject(10, :+)
 puts (5..10).my_inject(10, :*)
+
+puts "---multiply_els---"
+puts multiply_els([2,4,5])
